8e38230feea66f6b568d14f51bdf16a0003c084c
Vladislav <vladrosenrol@gmail.com>
2024-11-19 09:53:04 +0300

    доработал

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('api/', include('Cart.urls')),
     path('api/', include('Order.urls')),
     path('api/', include('Users.urls')),
-    path('api/', include('yokassa.urls')),
+    path('api/', include('yookassa.urls')),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('accounts/', include('allauth.urls')),
